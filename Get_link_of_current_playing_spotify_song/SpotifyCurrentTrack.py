5510f39622b651476e8e42dc5ab8723248cde7d6
vishnu4157 <88186341+vishnu4157@users.noreply.github.com>
2022-07-11 22:00:01 +0800

    Add files via upload

---
+++ Get_link_of_current_playing_spotify_song/SpotifyCurrentTrack.py
@@ -0,0 +1,47 @@
+import requests
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+
+ENDPOINT_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
+KEY = "BQDl01EEz1phvdKbuCAeAmijK-VWPsc1uKU9k8Yiv3mqDzEGnWC6vg0AXtxFen5GLDppx9-VS6J5Py55gY" \
+      "-iUtcGOozNHzrHvUqC_0MOsbgCRLSdFUFdDrF0MonQR1aCb6xgMgoKpYJYEZhWCwG_Ki_Dyty" \
+      "-4ugviza_gJ62OxBMDCLjt6F7neE5XoFnVmLRinuDsP79PqM_ "
+Token = "5437914227:AAHmLagra5J5tKfF2PaohR7uHoez_FdCWnM"
+updater = Updater("5437914227:AAHmLagra5J5tKfF2PaohR7uHoez_FdCWnM", use_context=True)
+
+def playing(update: Update, context: CallbackContext):
+    linkup = main()
+
+    update.message.reply_text(
+        f"{linkup}"
+        )
+    update.message.reply_text(
+        f"This is your current playing track^^^^"
+    )
+
+updater.dispatcher.add_handler(CommandHandler('playing', playing))
+
+updater.start_polling()
+
+
+def get_currently_playing(access_tok):
+    response = requests.get(
+        ENDPOINT_URL,
+        headers={
+            "Authorization": f"Bearer {access_tok}"
+        }
+    )
+    json_resp = response.json()
+    link = json_resp['item']['external_urls']['spotify']
+    track_link = {
+        "link": link
+    }
+    return track_link
+
+def main():
+    playing_track_link = get_currently_playing(KEY)
+    return playing_track_link.get("link")
+
+
